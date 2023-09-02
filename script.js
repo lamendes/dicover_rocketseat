d6301ad2b3de74beda31136b8b29054be7917f9d
Lucas Assis Mendes <lucas.a.mendes2@gmail.com>
2023-09-02 00:23:08 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("alt", "Foto do Lucas Mendes com oculos de sol");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "Foto do Lucas Mendes sem oculos de sol");
+  }
+}
